4987444e982abd4cc7f2d634f1d8cd8b55b4a447
Uhut Sancar <sancaruhut@gmail.com>
2026-08-24 16:57:42 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxt/content',
+    'nuxt-studio',
+    'motion-v/nuxt',
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+
+
+
+})
